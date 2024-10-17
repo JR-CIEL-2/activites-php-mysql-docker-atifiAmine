2bc619adf61e39ec373c5a55f37374cef2671f2d
ATIFI AMINE <atifi_a@JEAN-ROSTAND.lan>
2024-10-17 11:48:35 +0200

    Ex19

---
+++ Serie1/Site1/Ex19.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+function capital($country){
+    switch($country){
+        case  "France" :
+             return "Paris";
+        case  "Allemagne" :
+             return "Berlin";
+        case  "Italie" :
+             return "Rome";
+        case  "Maroc" : 
+            return "Rabat";
+        case  "Espagne" :
+             return "Madrid";
+        case  "Portugal" : 
+             return "Lisbonne";
+        case  "Angleterre" :
+             return "Londres";
+        default :
+         return "Inconnu";
+
+
+    }
+   
+}
+
+?>
