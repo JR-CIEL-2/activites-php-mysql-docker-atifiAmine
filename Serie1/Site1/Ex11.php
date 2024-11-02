cd0c95a920dac4030e20237813e3dfe9983569be
unknown <amine.atifi@lycee-jeanrostand.fr>
2024-11-02 22:17:28 +0100

    Ex11

---
+++ Serie1/Site1/Ex11.php
@@ -0,0 +1,12 @@
+<?php
+$argument1=1;
+$argument2=2;
+
+function plusPetit($argument1,$argument2){
+    if($argument1<$argument2){
+        return $argument1;
+    }else{
+        return $argument2;
+    }
+}
+?>
