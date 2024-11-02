c467ab3879f9821772ffc1c3bb8446477858777c
unknown <amine.atifi@lycee-jeanrostand.fr>
2024-11-02 22:16:53 +0100

    Ex10

---
+++ Serie1/Site1/Ex10.php
@@ -0,0 +1,12 @@
+<?php
+$argument1=1;
+$argument2=2;
+
+function plusGrand($argument1,$argument2){
+    if($argument1>$argument2){
+        return $argument1;
+    }else{
+        return $argument2;
+    }
+}
+?>
