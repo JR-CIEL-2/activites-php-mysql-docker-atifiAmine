14498e59dab5d614f799a55e47ff847ab5de2471
unknown <amine.atifi@lycee-jeanrostand.fr>
2024-11-03 16:28:18 +0100

    Ex6 (site2)

---
+++ Serie1/Site2/Ex6.php
@@ -0,0 +1,11 @@
+<?php
+
+$budget = 1553.89;
+$achats = 1554.76;
+if($budget>=$achats){
+    echo "Le budget permet de payer les achats";
+}else{
+    echo "Le budget ne permet pas de payer les achats";
+}
+
+?>
