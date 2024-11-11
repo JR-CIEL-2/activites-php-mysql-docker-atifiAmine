418dafc24aa1c92bcdca224d0157a6c3e0c643e6
atifiAmine <144659593+atifiAmine@users.noreply.github.com>
2024-11-11 21:12:10 +0000

    Ex13 (site2)

---
+++ Serie1/Site2/Ex13.php
@@ -0,0 +1,6 @@
+<?php
+$tableau = array("France" => "Paris", "Allemagne" => "Londres", "Italie" => "Rome");
+foreach ($tableau as $pays => $capitales){
+    echo $pays .  " a pour capitale " . $capitales . "<br>" ;
+}
+?>
