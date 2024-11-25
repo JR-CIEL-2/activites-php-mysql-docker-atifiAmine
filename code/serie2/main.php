a40138beca6a5b1406680dbec83a0c4388260c59
atifiAmine <144659593+atifiAmine@users.noreply.github.com>
2024-11-25 17:03:02 +0000

    Ex4.1(tri par valeur et reference

--- code/serie2/main.php
+++ code/serie2/main.php
@@ -1,16 +1,23 @@
 <?php
 include ("statistique.php");
 include("triangle.php");
+include("tri_selection.php");
 
 $notes=array(10,11,12);
 $salaires=array(1500,4500,2200,1500,3300,1800,1700,2000,4000);
 $resultat_moyenne= moyenne($notes);
 echo "La moyenne est de " .  $resultat_moyenne . '<br>';
 $resultat_salaire=mediane($salaires);
-echo " Le salaire median est de " . $resultat_salaire . "euros" . '<br>';
+echo " Le salaire median est de " . $resultat_salaire . " euros " . '<br>';
 
-$n = ($_GET['n']);
+$n = (int)$_GET['n'];
 echo triangle($n);
+
+echo implode(',',$salaires) . '<br>';
+
+tri_selection($salaires);
+echo implode(',',$salaires);
+
 
 
 ?>
